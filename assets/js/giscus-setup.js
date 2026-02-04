5ec9d676f271391944c5c36e1426473c705348a9
punhojark <62480393+punhojark@users.noreply.github.com>
2026-02-04 22:18:30 +0000

    Deploying to gh-pages from @ punhojark/punhojark.github.io@1992cdddce93b557266d494b0083d24541df396b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "punhojark/junhopark.github.io",
+    "data-repo": "punhojark/punhojark.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
